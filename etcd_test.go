5cc6944fe911f02071d918af266993c10a194ade
airingzeng <airingzeng@tencent.com>
2020-09-10 20:17:14 +0800

    add test for grpc

--- etcd_test.go
+++ etcd_test.go
@@ -1,12 +1,8 @@
 package air_etcd
 
 import (
-	"context"
-	"fmt"
 	"github.com/airingone/config"
 	"github.com/airingone/log"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/resolver"
 	"testing"
 	"time"
 )
@@ -55,25 +51,7 @@ func TestEtcdClient(t *testing.T) {
 	select {}
 }
 
-//grpc测试 todo
+//grpc测试
 func TestGrpcClient(t *testing.T) {
-	config.InitConfig()                     //配置文件初始化
-	log.InitLog(config.GetLogConfig("log")) //日志初始化
-
-	var endpoints []string //etcd集群的地址
-	endpoints = append(endpoints, "127.0.0.1:2380")
-	r := NewGrpcResolver(endpoints)
-	resolver.Register(r)
-
-	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
-	addr := fmt.Sprintf("%s:///%s", r.Scheme(), "g.srv.mail")
-	conn, err := grpc.DialContext(ctx, addr, grpc.WithInsecure(),
-		// grpc.WithBalancerName(roundrobin.Name),
-		//指定初始化round_robin => balancer (后续可以自行定制balancer和 register、resolver 同样的方式)
-		grpc.WithDefaultServiceConfig(`{"loadBalancingConfig": [{"round_robin":{}}]}`),
-		grpc.WithBlock())
-	defer conn.Close()
-	if err != nil {
-	}
-
+	//goto https://github.com/airingone/air-grpc/blob/master/grpc_test.go
 }
